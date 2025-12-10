f51cc8fd9cfddc2df5751bdbc3589fb0bc6f5317
Russell Jarvis <russelljarvis2008@gmail.com>
2025-12-09 17:55:53 -0700

    made working states for furhat

--- python/util/furhat.py
+++ python/util/furhat.py
@@ -1,3 +1,4 @@
+import logging
 from furhat_realtime_api import AsyncFurhatClient, FurhatClient
 import asyncio
 
@@ -5,7 +6,6 @@ import asyncio
 class Furhat:
   def __init__(self, ip: str):
     self.ip = ip
-    self.furhat_real = AsyncFurhatClient(ip)
-    asyncio.run(self.start_conversation())
-    print("furhat initialized")
-    
+    self.furhat_real: FurhatClient = FurhatClient(ip)
+    self.furhat_real.set_logging_level(logging.INFO)
+    self.furhat_real.connect()
